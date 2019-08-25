1ad0fde7322ad8abc83ac7d9ce87692cd0b3bce5
Yeongmin Kim <oinochoe@naver.com>
2019-08-25 19:56:12 +0900

    ssh test

--- styling-react/config/webpack.config.dev.js
+++ styling-react/config/webpack.config.dev.js
@@ -163,7 +163,7 @@ module.exports = {
               {
                 loader: require.resolve('css-loader'),
                 options: {
-                  importLoaders: 1,
+                  importLoaders: 1, 
                   // 19-08-25 css Module use setting
                   moudles: true,
                   localIdentName: '[path][name]__[local]--[hash:base64:5]'
