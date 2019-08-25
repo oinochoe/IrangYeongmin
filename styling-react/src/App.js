a312479e80382a447661abae3cf892fa922f012c
Yeongmin Kim <oinochoe@naver.com>
2019-08-25 20:10:03 +0900

    seeting again

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import styles from './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+console.log(styles); // 콘솔에 무억이 출력되나요
+
+class App extends Component () {
+  render() {
+    return (
+    <div className={styles.box}></div>
+    );
+  }
 }
 
 export default App;
