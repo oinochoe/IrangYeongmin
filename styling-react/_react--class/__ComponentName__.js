b61eeacaf4a25b3dd799c816db548c3e4f54e947
Yeongmin <oinochoe@naver.com>
2019-08-29 10:05:31 +0900

    include-media 설치, open-color 설치 , component 세팅 , button 컴포넌트 생성만

---
+++ styling-react/_react--class/__ComponentName__.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import styles from './__ComponentName__.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+class __ComponentName__ extends Component {
+  render() {
+    return (
+      <div>
+        __ComponentName__
+      </div>
+    );
+  }
+}
+
+export default __ComponentName__;
---
+++ styling-react/src/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Button.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const Button = () => (
+  <div>
+    Button
+  </div>
+);
+export default Button;
