8852ab8a6c86630e660c4df81b2582d5e260c997
Yeongmin Kim <oinochoe@naver.com>
2019-08-31 16:58:48 +0900

    component 정리 및 따로 분할, todolist 만드는중 컴포넌트들 완성 및 기본적인 상태관리 시작

--- todo-list/src/components/App.js
+++ todo-list/src/components/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import PageTemplate from './PageTemplate';
+import TodoInput from './TodoInput';
+import TodoList from './TodoList';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        일정관리
-      </div>
+      <PageTemplate>
+        <TodoInput/>
+        <TodoList/>
+      </PageTemplate>
     );
   }
 }
