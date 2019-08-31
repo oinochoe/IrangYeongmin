d818eea1c0e344d980babe5a21572d23a79d60db
Yeongmin Kim <oinochoe@naver.com>
2019-08-31 21:15:28 +0900

    todo item done이 바뀔 때만 업데이트하도록 최적화

--- todo-list/src/components/TodoItem/TodoItem.js
+++ todo-list/src/components/TodoItem/TodoItem.js
@@ -5,6 +5,10 @@ import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
 class TodoItem extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+      return this.props.done !== nextProps.done;
+  }
+  
   render() {
     const {done, children, onToggle, onRemove} = this.props;
     /* 앞 코드에서는 비구조화 할당을 이용해서 this.props 안에 있는 
