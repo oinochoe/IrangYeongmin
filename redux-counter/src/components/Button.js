81f02189e9a5c00ca172a574ecc53eee0b3b4bec
Yeongmin Kim <oinochoe@naver.com>
2019-09-01 21:09:49 +0900

    utf-8로 수정

--- redux-counter/src/components/Button.js
+++ redux-counter/src/components/Button.js
@@ -6,7 +6,7 @@ import './Button.css';
 const Buttons = ({onCreate, onRemove}) => {
   return (
     <div className="Buttons">
-      <div className="btn add" onClick={onCreate}>생성</div>
+      <div className="btn add" onClick={onCreate}>추가</div>
       <div className="btn remove" onClick={onRemove}>제거</div>
     </div>
   );
