8eb0a22bd02a7870a1a7a149c5d8cfcbaecf54ff
Yeongmin <oinochoe@naver.com>
2019-09-03 10:01:34 +0900

    Ducks 구조로 작성한 리덕스

---
+++ redux-counter/src/modules/input.js
@@ -0,0 +1,14 @@
+import { Map } from 'immutable';
+import { handleActions, createAction } from 'redux-actions';
+
+const SET_INPUT = 'input/SET_INPUT';
+
+const initialState = Map({
+  value: ''
+});
+
+export default handleActions({
+  [SET_INPUT]: (state, action) => {
+    return state.set('value', action.payload)
+  }
+}, initialState)
