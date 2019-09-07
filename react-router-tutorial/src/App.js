add8bb874decf9dfe1938c7e84a338b7da2efa76
Yeongmin Kim <oinochoe@naver.com>
2019-09-08 00:35:35 +0900

    react 메뉴 설정

--- react-router-tutorial/src/App.js
+++ react-router-tutorial/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
+import {
+  Home,
+  About
+} from 'pages';
+import Menu from 'components/Menu';
 
 const App = () => {
   return (
-    <div>리액트 라우터를 배워봅시다.</div>
+    <div>
+      <Menu/>
+      <Route exact path="/" component={Home}/>
+      <Route path="/about/:name?" component={About} />
+    </div>
   );
 };
 
