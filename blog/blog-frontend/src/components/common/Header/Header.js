cb36dd9c0c817479276826d04ab319749a9b7f8b
Yeongmin Kim <oinochoe@naver.com>
2019-09-22 18:36:15 +0900

    로그인 여부에 따른 컴포넌트 on, off 처리 및 코드스플리팅 예비작업

--- blog/blog-frontend/src/components/common/Header/Header.js
+++ blog/blog-frontend/src/components/common/Header/Header.js
@@ -6,22 +6,24 @@ import Button from 'components/common/Button';
 
 const cx = classNames.bind(styles);
 
-const Header = ({postId, onRemove}) => (
+const Header = ({postId, logged, onRemove}) => (
   <header className={cx('header')}>
     <div className={cx('header-content')}>
       <div className={cx('brand')}>
         <Link to="/">reactblog</Link>
       </div>
-      <div className={cx('right')}>
-        {
-          // flex를 유지하려고 배열 형태로 렌더링합니다.
-          postId && [
-            <Button key="edit" theme="outline" to={`/editor?id=${postId}`}>수정</Button>,
-            <Button key="remove" theme="outline" onClick={onRemove}>삭제</Button>
-          ]
-        }
-        <Button theme="outline" to="/editor">새 포스트</Button>
-      </div>
+      { logged &&
+        <div className={cx('right')}>
+          {
+            // flex를 유지하려고 배열 형태로 렌더링합니다.
+            postId && [
+              <Button key="edit" theme="outline" to={`/editor?id=${postId}`}>수정</Button>,
+              <Button key="remove" theme="outline" onClick={onRemove}>삭제</Button>
+            ]
+          }
+          <Button theme="outline" to="/editor">새 포스트</Button>
+        </div>
+      }
     </div>
   </header>
 );
