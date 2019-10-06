0350ba198bb737f306a6f0d615e6cb20f258ca15
Yeongmin Kim <oinochoe@naver.com>
2019-10-06 19:23:52 +0900

    edit published date

--- blog/blog-frontend/src/components/post/PostInfo/PostInfo.js
+++ blog/blog-frontend/src/components/post/PostInfo/PostInfo.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import styles from './PostInfo.scss';
-import classNames from 'classnames/bind';
+import React from "react";
+import styles from "./PostInfo.scss";
+import classNames from "classnames/bind";
 
-import { Link } from 'react-router-dom';
-import moment from 'moment';
+import { Link } from "react-router-dom";
+import moment from "moment";
 
 const cx = classNames.bind(styles);
 
-const PostInfo = ({publishedDdate, title, tags}) => (
-  <div className={cx('post-info')}>
-    <div className={cx('info')}>
+const PostInfo = ({ publishedDdate, title, tags }) => (
+  <div className={cx("post-info")}>
+    <div className={cx("info")}>
       <h1>{title}</h1>
-      <div className={cx('tags')}>
-        {
-          // tags가 존재할 때만 map을 실행합니다.
-          tags && tags.map(
-            tag => <Link key={tag} to={`/tag/${tag}`}>#{tag}</Link>
-          )
-        }
+      <div className={cx("tags")}>
+        {// tags가 존재할 때만 map을 실행합니다.
+        tags &&
+          tags.map(tag => (
+            <Link key={tag} to={`/tag/${tag}`}>
+              #{tag}
+            </Link>
+          ))}
       </div>
-      <div className={cx('date')}>{moment(publishedDdate).format('ll')}</div>
+      {/* <div className={cx("date")}>{moment(publishedDdate)}</div> */}
+      <div className={cx("date")}>{publishedDdate}</div>
     </div>
   </div>
 );
